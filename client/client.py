66bf97f857acf4f27140e675086ffbd4ee9885d8
vasiliy <vaszol@mail.ru>
2021-07-08 23:07:40 +0300

    Добавлен шаблон и файл чата

--- client/client.py
+++ client/client.py
@@ -1,22 +1,37 @@
 import socket
+import json
+
+chat = []
+my_msg = []
+
+with open('config.json') as json_file:
+    config = json.load(json_file)
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.connect(('192.168.1.46', 5555))
-print('Client => 192.168.1.46:5555')
-
-config = {
-    'name':'Super'
-}
-
+client_socket.connect((config['ip'], config['port']))
 connection_msg = '< ' + config['name'] + ' connected >'
 client_socket.sendall(str.encode(connection_msg))
+my_msg.append(connection_msg)
 
-while True:
-    data = client_socket.recv(1024)
+with open('template.txt') as file:
+    template_reset = file.read()
 
-    print(data.decode('utf-8'))
+with open('chat.txt', 'w') as file:
+    file.write('<! You logged in as: ' + config['name'] + ' !>\n')
+    file.write(template_reset)
 
-    if not data:
-        break
+try:
+    while True:
+        data = client_socket.recv(1024)
 
-client_socket.close()
+        print(data.decode('utf-8'))
+
+        if not data:
+            break
+except KeyboardInterrupt:
+    disconnection_msg = '< ' + config['name'] + ' disconnected >'
+    client_socket.sendall(str.encode(disconnection_msg))
+    my_msg.append(disconnection_msg)
+    client_socket.close()
+    with open('chat.txt', 'w') as file:
+        file.write('')
